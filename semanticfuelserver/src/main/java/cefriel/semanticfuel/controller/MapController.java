c3d7bd854779f76ee7562ec7aeb337fb39217cae
antoniogianola <antonio.gianola@tinvention.com>
2019-10-09 23:04:36 +0200

    init webapp

--- semanticfuelserver/src/main/java/cefriel/semanticfuel/controller/MapController.java
+++ semanticfuelserver/src/main/java/cefriel/semanticfuel/controller/MapController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cefriel.semanticfuel.service.map.OpenStreetDirectionService;
@@ -12,6 +13,7 @@ import cefriel.semanticfuel.service.map.OpenStreetPlacesService;
 import model.Place;
 
 @RestController
+@RequestMapping("/api")
 public class MapController {
 
 	@Autowired
