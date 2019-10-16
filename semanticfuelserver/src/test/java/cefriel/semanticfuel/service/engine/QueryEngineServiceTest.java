3aad61918091572949791fb5c63acfab44284acb
tod <michele.todero@gmail.com>
2019-10-16 21:01:25 +0200

    things

---
+++ semanticfuelserver/src/test/java/cefriel/semanticfuel/service/engine/QueryEngineServiceTest.java
@@ -0,0 +1,16 @@
+package cefriel.semanticfuel.service.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cefriel.semanticfuel.utils.AbstractTest;
+
+public class QueryEngineServiceTest extends AbstractTest {
+	@Autowired
+	private QueryEngineService engine;
+
+	@Test
+	public void updateOntology() {
+		engine.updateOntology();
+	}
+};
