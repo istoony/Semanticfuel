6e9ebbea8afd6b662359ef5cbf9d52000b87c71b
tod <michele.todero@gmail.com>
2019-10-17 22:21:02 +0200

    few fixes, still buggy
    - problem with original dataset: some coordinates are missing
    - need to change rml mapper to create geometries
    - geo query needs an ontology for unit of measures

--- semanticfuelserver/src/main/java/cefriel/semanticfuel/service/engine/QueryEngineService.java
+++ semanticfuelserver/src/main/java/cefriel/semanticfuel/service/engine/QueryEngineService.java
@@ -2,7 +2,6 @@ package cefriel.semanticfuel.service.engine;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +12,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.jena.geosparql.configuration.GeoSPARQLConfig;
 import org.apache.jena.geosparql.spatial.SpatialIndex;
 import org.apache.jena.geosparql.spatial.SpatialIndexException;
+import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -21,8 +21,6 @@ import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.StmtIterator;
-import org.eclipse.rdf4j.model.Namespace;
 import org.eclipse.rdf4j.rio.RDFFormat;
 
 import be.ugent.rml.Executor;
@@ -31,7 +29,6 @@ import be.ugent.rml.functions.FunctionLoader;
 import be.ugent.rml.functions.lib.GrelProcessor;
 import be.ugent.rml.functions.lib.IDLabFunctions;
 import be.ugent.rml.records.RecordsFactory;
-import be.ugent.rml.store.Quad;
 import be.ugent.rml.store.QuadStore;
 import be.ugent.rml.store.RDF4JStore;
 import be.ugent.rml.term.Term;
@@ -43,24 +40,22 @@ public class QueryEngineService extends AbstractService {
 	}
 
 	public void updateOntology() throws IOException, SpatialIndexException {
-		Pair<QuadStore, RDF4JStore> result = runRMLParser();
-		QuadStore tripleList = result.getLeft();
-		RDF4JStore rmlStore = result.getRight();
-
-		List<Quad> triples = tripleList.getQuads(null, null, null);
-
-//		triples.forEach(a -> System.out.println(
-		// a.getSubject().toString() + " " + a.getPredicate().toString() + " " +
-		// a.getObject().toString()));
-
-		tripleList.write(new FileWriter("prova.ttl"), "turtle");
 		/**
-		 * Model model = ModelFactory.createDefaultModel(); model.set for (Quad q :
-		 * triples) { Resource res = model.createResource(q.getSubject().getValue());
+		 * Pair<QuadStore, RDF4JStore> result = runRMLParser(); QuadStore tripleList =
+		 * result.getLeft(); RDF4JStore rmlStore = result.getRight();
+		 * 
+		 * List<Quad> triples = tripleList.getQuads(null, null, null);
+		 * 
+		 * // triples.forEach(a -> System.out.println( // a.getSubject().toString() + "
+		 * " + a.getPredicate().toString() + " " + // a.getObject().toString()));
+		 * 
+		 * tripleList.write(new FileWriter("prova.ttl"), "turtle"); /** Model model =
+		 * ModelFactory.createDefaultModel(); model.set for (Quad q : triples) {
+		 * Resource res = model.createResource(q.getSubject().getValue());
 		 * res.addProperty(model.createProperty(q.getPredicate().getValue()),
 		 * q.getObject().getValue()); }
+		 * 
 		 **/
-
 		Model model = ModelFactory.createDefaultModel();
 		try {
 			model.read(new FileInputStream("prova.ttl"), null, "TTL");
@@ -68,21 +63,25 @@ public class QueryEngineService extends AbstractService {
 			e.printStackTrace();
 		}
 
-		for (Namespace ns : rmlStore.getNamespaces()) {
-			model.setNsPrefix(ns.getPrefix(), ns.getName());
-			System.out.println(ns.getPrefix() + " " + ns.getName());
-		}
-
-		StmtIterator si = model.listStatements();
-		while (si.hasNext()) {
-			System.out.println(si.next());
-		}
+		/**
+		 * for (Namespace ns : rmlStore.getNamespaces()) {
+		 * model.setNsPrefix(ns.getPrefix(), ns.getName());
+		 * System.out.println(ns.getPrefix() + " " + ns.getName()); }
+		 * 
+		 * 
+		 * StmtIterator si = model.listStatements(); while (si.hasNext()) { //
+		 * System.out.println(si.next()); }
+		 **/
 
 		// Create a new query
 		String queryString = "PREFIX gso:  <http://gas_station.example.com/data#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
 				+ "SELECT ?station " + "WHERE {" + "      ?station rdf:type gso:gas_station}";
 
-		Query query = QueryFactory.create(queryString);
+		String coneQuery = "PREFIX gso:  <http://gas_station.example.com/data#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "SELECT ?station " + "WHERE {"
+				+ "      ?station gso:has_address ?address . ?address gso:city 'CONEGLIANO'}";
+
+		Query query = QueryFactory.create(coneQuery);
 
 		// Execute the query and obtain results
 		QueryExecution qe = QueryExecutionFactory.create(query, model);
@@ -95,19 +94,25 @@ public class QueryEngineService extends AbstractService {
 		// Important ‑ free up resources used running the query
 		qe.close();
 
+		boolean stop = false;
+		if (stop)
+			return;
+
 		System.out.println("yeye");
 
 		GeoSPARQLConfig.setupMemoryIndex();
-		SpatialIndex.wrapModel(model);
-		queryString = "PREFIX spatial: <http://jena.apache.org/spatial#> PREFIX gso:  <http://gas_station.example.com/data#> "
+		Dataset spatialDataset = SpatialIndex.wrapModel(model);
+		queryString = "PREFIX spatial: <http://jena.apache.org/spatial#>"
+				+ " PREFIX uom: <http://www.w3.org/2007/ont/unit#>"
+				+ " PREFIX gso:  <http://gas_station.example.com/data#> "
 				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " + "SELECT ?placeName" + " WHERE{"
-				+ "    ?place spatial:nearby (13.60 3.71 1 'km') . ?place rdf:type gso:gas_station}";
+				+ "    ?place spatial:nearby (13.60 3.71 1 uom:km) . ?place rdf:type gso:gas_station}";
 
 		System.out.println("yoyo");
 
-		try (QueryExecution qe1 = QueryExecutionFactory.create(queryString, model)) {
+		try (QueryExecution qe1 = QueryExecutionFactory.create(queryString, spatialDataset)) {
 			ResultSet rs = qe1.execSelect();
-			ResultSetFormatter.out(System.out, rs, query);
+			ResultSetFormatter.out(System.out, rs, qe1.getQuery());
 		}
 	}
 
